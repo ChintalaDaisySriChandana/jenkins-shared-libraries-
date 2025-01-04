0d75f99fd139b0c7547aa0412f41ec341aad58f7
Chintala_Daisy_Sri_Chandana <81558050+ChintalaDaisySriChandana@users.noreply.github.com>
2025-01-04 23:09:36 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,24 +1,19 @@
-def check_out() {
+def call() {
+
     echo 'Checking out code...'
     checkout scm
-}
-
-def build_project() {
+    echo 'Setting up Java 17...'
+    sh 'sudo apt update'
+    sh 'sudo apt install -y openjdk-17-jdk'
     echo 'Building project with Maven...'
     sh 'mvn clean package'
-}
-
-def run_application() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
 
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
-}
-
-def validate_app() {
-    echo 'Validating that the app is running...'
+  echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
         echo 'The app is running successfully!'
@@ -26,9 +21,6 @@ def validate_app() {
         echo "The app failed to start. HTTP response code: ${response}"
         error("The app did not start correctly!")
     }
-}
-
-def spring_boot() {
-    echo 'Gracefully stopping the Spring Boot application...'
-    sh 'mvn spring-boot:stop'
+      echo 'Gracefully stopping the Spring Boot application...'
+      sh 'mvn spring-boot:stop' 
 }
