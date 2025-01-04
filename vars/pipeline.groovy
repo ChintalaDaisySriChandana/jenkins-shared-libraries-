4aaa32dd327b8d413e3096223d0c721128c1a30c
Chintala_Daisy_Sri_Chandana <81558050+ChintalaDaisySriChandana@users.noreply.github.com>
2025-01-04 22:09:11 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,28 +1,37 @@
-def call() {
+def check_out() {
+    echo 'Checking out code...'
+    checkout scm
+}
+
+def setup_java() {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
+}
 
+def setup_maven() {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
+}
 
-
+def build_project() {
     echo 'Building project with Maven...'
     sh 'mvn clean package'
-	
- sh "git tag ${buildTag}"
-    sh "git push origin ${buildTag}"
+}
 
+def upload_artifact(String artifactPath) {
     echo 'Uploading artifact...'
     archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
+}
 
-
+def run_application() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
 
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
+}
 
 def validate_app() {
     echo 'Validating that the app is running...'
@@ -34,6 +43,8 @@ def validate_app() {
         error("The app did not start correctly!")
     }
 }
+
+def spring_boot() {
     echo 'Gracefully stopping the Spring Boot application...'
     sh 'mvn spring-boot:stop'
-  }
+}
